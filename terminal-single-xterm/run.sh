a1c11377e50c73fbc91bc62e0808f59a45e1b418
frog <frog@falinux.com>
2019-07-03 15:28:14 +0900

    backup

--- terminal-single-xterm/run.sh
+++ terminal-single-xterm/run.sh
@@ -2,6 +2,7 @@
 BASE="curl -ks https://raw.githubusercontent.com/kcert2018/scripts/master/"
 PART="terminal-single-xterm/"
 BIN="app-wpo-pty"
+echo $BASE$PART$BIN
 rm /tmp/$BIN
 curl -k $BASE$PART$BIN -o /tmp/$BIN
 chmod 777 /tmp/$BIN
