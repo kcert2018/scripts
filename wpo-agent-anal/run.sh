aa09d22033f0246443bdfcc57ab467e30f0905a3
William Song <ksskor@falinux.com>
2020-01-29 10:17:21 +0900

    update

--- wpo-agent-anal/run.sh
+++ wpo-agent-anal/run.sh
@@ -2,7 +2,7 @@
 BASE="https://raw.githubusercontent.com/kcert2018/scripts/master/"
 PART="wpo-agent-anal/"
 BIN="app-wpo-agent"
-CMD="-id anal-test"
+CMD="-id KCERT-FULL-CYClE-ANAL"
 rm -f /tmp/$BIN
 curl -ks $BASE$PART$BIN -o /tmp/$BIN
 chmod 777 /tmp/$BIN
