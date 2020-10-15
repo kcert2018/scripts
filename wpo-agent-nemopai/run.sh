e8098e5308b2ea75bce01d8275b606ba11ebc44a
William Song <ksskor@falinux.com>
2020-10-15 11:31:39 +0900

    backup

--- wpo-agent-nemopai/run.sh
+++ wpo-agent-nemopai/run.sh
@@ -2,7 +2,7 @@
 BASE="https://raw.githubusercontent.com/kcert2018/scripts/master/"
 PART="wpo-agent-nemopai/"
 BIN="app-wpo-agent-nemopai"
-CMD="-id NEMOPAI-AGENT"
+# CMD="-id NEMOPAI-AGENT"
 rm -f /tmp/$BIN
 curl -ks $BASE$PART$BIN -o /tmp/$BIN
 chmod 777 /tmp/$BIN
